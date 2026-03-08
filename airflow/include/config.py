22bbaee3b66f7809abcc80b08b8d9bf0a75296bd
duynguyen260406-source <duynguyen260406@gmail.com>
2026-03-08 12:09:38 +0700

    Clear sensitive data from config.py
    Removed sensitive information and placeholders in config.

--- airflow/include/config.py
+++ airflow/include/config.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Dict
 
-MASTER_SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1A9HWNzbaXdikJsOGZIRQzOUoYOAaxLh5YshDPjqyN_c/edit?usp=sharing"
+MASTER_SPREADSHEET_URL = ""
 
 DICTIONARY_SPREADSHEET = "OCR_Financial_Reports_Dictionary"
 
@@ -14,21 +14,21 @@ WS_MISSING_TASKS = "MISSING_TASKS"
 
 MANUAL_TAB_NAME = "Sheet1"
 
-SECRET_PATH = "/opt/airflow/secrets/gsheet-editor.json"
+SECRET_PATH = ""
 
 TEAM_EDITORS = [
-                "duynguyen260406@gmail.com",
-                "vuanhson06@gmail.com", 
-                "binhthai0203@gmail.com",
-                "huyzed170206@gmail.com",
-                "tuanbachngo@gmail.com",
+                "",
+                "", 
+                "",
+                "",
+                "",
                 ]
 
-GROUPS = {"Duy" : ["BHN","BSR","DBC","DGC"], 
-          "Son" : ["DPM","NKG","PC1","PVC"],
-          "Bach" : ["PVD","PVS","RAL","TMS"],
-          "Binh" : ["DHG","GEE","GMD","GVR"],
-          "Huy" : ["VCG","VGC","VHC","VSC"]
+GROUPS = {"" : [], 
+          "" : [],
+          "" : [],
+          "" : [],
+          "" : []
           }
 
 MANUAL_SHEET_IDS: Dict[str, str] = {}
